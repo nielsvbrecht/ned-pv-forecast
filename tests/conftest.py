e4dda0e966eb964698c2b5226b6313a1aefdc4bd
Niels van Brecht <github@vanbrecht.family>
2025-07-15 16:13:41 +0200

    update from test feedback

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,7 +7,7 @@ pytest_plugins = "pytest_homeassistant_custom_component"
 @pytest.fixture(autouse=True)
 def auto_enable_custom_integrations(enable_custom_integrations):
     """Enable custom integrations for all tests."""
-    yield
+    return
 
 @pytest.fixture
 def mock_api_response():
